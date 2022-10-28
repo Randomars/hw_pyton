ed000b1c9fe9be5e338a69039d2861d88ee55823
Arseny <golem9@mail.ru>
2022-10-28 18:40:41 +0300

    HW4

--- hw01.py
+++ hw01.py
@@ -1,102 +1,230 @@
-# 1. Сумма нечетных элементов
-
-# numbers = ['2','3','5','9','3']
-
-# summ = 0
-# for i in range(1,len(numbers),2):
-#     summ += int(numbers[i])
-# print(f'Исходные числа:{numbers}')
-# print(f'Сумма нечетных элементов: {summ}')
-
-
-# 2. Произведения  пар
-
-# numbers = ['2','3','4','5','6']
-
-# i = 0
-# produkt = []
-
-# while i < len(numbers)/2:
-#     produkt.append(int(numbers[i])*int(numbers[len(numbers)-i-1]))
-#     i += 1
-# print(f'Исходные числа:{numbers}')
-# print(f'Произведение пар чисел: {produkt}')
-
-
-# 3. Разница дробных частей
-
-# numbers = [1.1, 1.2, 3.1, 5, 10.01]
-
-# minFrac:float = 0
-# maxFrac:float = 0
-
-# for i in numbers:
-#     if i % 1 > 0:
-#         minFrac = i % 1
-#         maxFrac = i % 1
-#         break
-
-# for i in numbers:
-#     if i % 1 > 0:
-#         if i % 1 < minFrac:
-#             minFrac = i % 1
-#         if i % 1 > maxFrac:
-#             maxFrac = i % 1
-
-# print(f'Исходные числа:{numbers}')
-# print(f'Разница между значениями дробных частей: {maxFrac - minFrac}')
-
-# -= remark =-
-
-# from decimal import Decimal # Для более точного написания чисел с плавающей точкой.
-
-# print(max(numbers) - min(numbers))  # Более легкий способ выделить мин и макс из списка.
-# num = Decimal('5.42')
-# print(num)
-
-
-# 4. Преобразование в двоичное число.
-
-# a = int(input("Введите число: "))
-# binaryRes = ''
-
-# while a / 2 > 0:
-#     if a%2 == 0:
-#         binaryRes = '0' + binaryRes
-#     else:
-#         binaryRes = '1' + binaryRes
-#     a = int(a/2)
-
-# print('Это число в двоичной системе: ' + binaryRes)
-
-
-# 5. Список чисел Фибоначчи
-
-# Вариант 1
-
-# def Fibonacci(n: int):
-#     if (n == 1) or (n == 2):
-#         return 1
-#     else:
-#         if n > 0:
-#             return Fibonacci(n - 1) + Fibonacci(n - 2)
-#         else:
-#             return Fibonacci(n + 2) - Fibonacci(n + 1)
-
-# a = int(input("Введите значение индекса: "))
-# rowFibonacci = []
-# for i in range(-a,a+1):
-#     rowFibonacci.append(Fibonacci(i))
-
-# print(rowFibonacci)
-
-# # Вариант 2
+# Привет, Стоун!
+# Язык питон такой легкий, что на нем надо писать совсем не так как на освоенном C#. 
+# Спасибо за подсказку, что новое задание надо делать с использованием словарей. 
+# У меня сразу появились идеи для решения, а то на примере прошлого ДЗ для Фибоначчи я брал идею из варината на C#.
+# А после обсуждения на семинаре, у меня получилось вот такое элегантное решение (там только формальный косяк при индексе 0):
 
 # a = int(input("Введите значение индекса: "))
 # rF = [1, 0, 1]
 
-# for i in range(2, a + 1):
+# for _ in range(2, a + 1):
 #     rF.append(rF[len(rF)-1] + rF[len(rF)-2])
 #     rF.insert(0, rF[1] - rF[0])
 
 # print(rF)
+
+# Вобще, после лекции, непонятна область применения всех этих вариантов списков (кортежей, множеств и тд), 
+# и в голову идут "стандартные" решения C#, а тут надо делать изящнее, по питоновски.
+# Спасибо за проверку работ:). А теперь новое ДЗ.
+
+# P.S. Ой что-то я умаялся с выполнением. Поду смотреть решения.
+
+
+
+# 1. Число ПИ
+
+# import math
+
+# a = float(input("Введите точность числа ПИ (в формате 0.001): "))
+# if a == 0:
+#     exit()
+
+# i = 0
+# while a < 1:
+#     i += 1
+#     a *= 10
+# print(round(math.pi, i))
+
+
+# 2. Простые множители числа.
+
+# Вариант 1.
+
+# a = int(input("Введите число: "))
+# N = a
+
+# print('Строим ряд простых чисел')
+# simplList = [2]
+
+# for i in range(3, a//2+1, 2):
+#     mark = True
+#     for j in simplList:
+#         if i % j == 0:
+#             mark = False
+#     if mark == True:
+#         simplList.append(i)
+
+# print('Выбираем множители заданного числа')
+# numSimpleList = []
+
+# while a > 1:
+#     for i in simplList:
+#         if a % i == 0:
+#             numSimpleList.append(i)
+#             a = int(a / i)
+
+# print(f'Список простых множителей числа {N}:')
+# print(numSimpleList)
+
+# Вариант 2. Оказалось всё проще.
+
+# a = int(input("Введите число: "))
+# N = a
+
+# numSimpleList = []
+# i = 2
+
+# while a > 1:
+#     while a % i == 0:
+#         a = a // i
+#         numSimpleList.append(i)
+#     i += 1
+
+# print(f'Список простых множителей числа {N}:')
+# print(numSimpleList)
+
+
+# 3. Список неповторяющихся элементов последовательности
+
+# from random import randint as rnd
+
+# l = rnd(10,20)
+# row0 = []
+
+# for _ in range(l):
+#     row0.append(rnd(0,9))
+
+# strInput = ''
+# for i in range(len(row0)):
+#     strInput += str(row0[i])
+
+# rowStart = []
+# rowEnd = []
+# for i in strInput:
+#     if i not in rowStart:
+#         rowEnd.append(i)
+#     elif len(rowEnd) > 0:
+#         j = 0
+#         while j < len(rowEnd):
+#             if rowEnd[j] == i:
+#                 rowEnd.pop(j)
+#             j += 1
+#     rowStart.append(i)
+
+# print('В последовательности:')
+# print(strInput)
+# print('Неповторяющиеся элементы:')
+# print(rowEnd)
+
+# 4. Запись многочлена степени k.
+
+# from random import randint as rnd
+
+# k = int(input("Введите степень многочлена: "))
+
+# koefRange = 100
+# mnogochlenD = {}
+# mnogochlenStr = ''
+# mnogochlenD[k] = 0
+
+# while mnogochlenD[k] == 0:
+#     mnogochlenD[k] = rnd(-koefRange,koefRange)
+
+# for i in range(k):
+#     koef = rnd(-koefRange,koefRange)
+#     if koef != 0:
+#         mnogochlenD[i] = koef
+
+# for i in range(k,-1,-1):
+#     if i in mnogochlenD:
+#         if i == k:
+#             mnogochlenStr = str(mnogochlenD[i])
+#         elif mnogochlenD[i] > 0:
+#             mnogochlenStr = mnogochlenStr + ' + ' + str(mnogochlenD[i])
+#         elif  mnogochlenD[i] < 0:
+#             mnogochlenStr = mnogochlenStr + ' - ' + str(abs(mnogochlenD[i]))
+    
+#         if i == 1:
+#             mnogochlenStr = mnogochlenStr + '*x'
+#         elif i != 0:
+#             mnogochlenStr = mnogochlenStr + '*x**' + str(i)
+# else:
+#     mnogochlenStr = mnogochlenStr + ' = 0'
+
+# print(f'Многочлен: {mnogochlenStr}')
+
+# path = r'text.txt'
+
+# with open(path, 'w', encoding='UTF-8') as data:
+#     data.write(mnogochlenStr)
+# print(f'Сохнанен в файл "{path}"')
+
+
+# 5. Сложение 2х многочленов
+
+# path1 = r'text1.txt'
+# path2 = r'text2.txt'
+# path3 = r'solution.txt'
+
+
+# def ToDict(textIn: str):
+#     mnogoD = {}
+#     for i in textIn:
+#         if i.find('*x**') > 0:
+#             dataBit = i.split('*x**')
+#             mnogoD[int(dataBit[1])] = int(dataBit[0])
+#         elif i.find('*x') > 0:
+#             mnogoD[1] = int(i.replace('*x', ''))
+#         else:
+#             mnogoD[0] = int(i)
+#     return mnogoD
+
+
+# with open(path1, 'r', encoding='UTF-8') as data:
+#     file1 = data.readline()
+# mnogoD1 = ToDict(file1.replace(' = 0', '').replace(' + ', ' ').replace(' - ', ' -').split())
+    
+# with open(path2, 'r', encoding='UTF-8') as data:
+#     file2 = data.readline()
+# mnogoD2 = ToDict(file2.replace(' = 0', '').replace(' + ', ' ').replace(' - ', ' -').split())
+
+# mnogoD3 = {}
+# for i in mnogoD1:
+#     mnogoD3[i] = mnogoD1[i] + mnogoD2[i]
+
+
+# for i in mnogoD3.keys():
+#     k = i
+
+# for i in mnogoD3.keys():
+#     if k < i:
+#         k = i
+
+# mnogochlenStr = ''
+
+# for i in range(k,-1,-1):
+#     if i in mnogoD3:
+#         if i == k:
+#             mnogochlenStr = str(mnogoD3[i])
+#         elif mnogoD3[i] > 0:
+#             mnogochlenStr = mnogochlenStr + ' + ' + str(mnogoD3[i])
+#         elif  mnogoD3[i] < 0:
+#             mnogochlenStr = mnogochlenStr + ' - ' + str(abs(mnogoD3[i]))
+    
+#         if i == 1:
+#             mnogochlenStr = mnogochlenStr + '*x'
+#         elif i != 0:
+#             mnogochlenStr = mnogochlenStr + '*x**' + str(i)
+# else:
+#     mnogochlenStr = mnogochlenStr + ' = 0'
+
+# print('Сумма многочленов:')
+# print(file1)
+# print(file2)
+# print('Получена в виде:')
+# print(mnogochlenStr)
+
+# with open(path3, 'w', encoding='UTF-8') as data:
+#     data.write(mnogochlenStr)
+# print(f'Результат сохнанен в файл "{path3}"')
+
